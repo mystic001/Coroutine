cbdbd209c220a373620277ba1bc2e8e7ffa0889b
mystic <bamideleomonayin@gmail.com>
2021-06-06 14:30:24 +0000

    first commit

---
+++ src/main/kotlin/Beats.kt
@@ -0,0 +1,32 @@
+import java.io.File
+import javax.sound.sampled.AudioSystem
+
+fun playBeats(beats: String, file: String ){
+    val parts = beats.split("x")
+    var count = 0
+    for(part in parts ){
+        count += part.length+1
+        if(part == ""){
+            playSound(file)
+        }else{
+            Thread.sleep(100*(part.length +1L))
+            if(count < beats.length){
+                playSound(file)
+            }
+        }
+    }
+}
+
+
+fun playSound(file: String){
+   val clip = AudioSystem.getClip()
+   val audioInputStream = AudioSystem.getAudioInputStream(File(file))
+    clip.open(audioInputStream)
+    clip.start()
+}
+
+
+fun main(){
+    playBeats("tom-tom-tom","toms.aiff")
+    playBeats("cra-cra-cras","crash_cymbal.aiff")
+}
